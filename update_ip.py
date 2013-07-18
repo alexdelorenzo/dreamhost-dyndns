dd0d19c0a71c5a3047e123ad91f6d750e715e7f6
root <root@exynos.lan>
2013-07-18 19:34:48 +0000

    refactored module, removed redundant code

--- update_ip.py
+++ update_ip.py
@@ -2,8 +2,7 @@ import getip
 import dhlib
 import csv
 
-domain_file = 'domains.csv'
-delim = ','
+domain_file, delim = 'domains.csv', ','
 to_be_culled = []
 
 def _grab_ip():
@@ -12,24 +11,22 @@ def _grab_ip():
  
 def _connect_api(server, key):
 	connection = dhlib._connect(server, key)
+
 	print "Connected to DH API: ", connection.IsConnected(), "(", server, ")"
+
 	if connection.IsConnected() is False:
-		print "Not able to connect. Check your server name and API key values."
+		print "Unable to connect. Check server name and API key values."
 		exit()
 
 	return connection
 
-def _to_be_culled(connection=None, record=None, value=None):
-	if record is None or value is None or connection is None:
-		culling_inevitable = True if len(to_be_culled) is 0 else False
-		return culling_inevitable
-	else:
-		to_be_culled.append((connection, record, value))
+def _to_be_culled(connection, record, value):
+	to_be_culled.append((connection, record, value))
 
 def _check_record(connection, server, ipaddr):
 	records = connection.dns.list_records()
 
-	no_records = True if len(records) is 0 else False	
+	no_records = len(records) is 0	
 	
 	if no_records:
 		return True
@@ -39,6 +36,7 @@ def _check_record(connection, server, ipaddr):
 			name, value = record['record'], record['value']
 		except TypeError:
 			print records
+			return False
 		else:
 			if name == server and value == str(ipaddr):
 				return False
@@ -49,15 +47,20 @@ def _check_record(connection, server, ipaddr):
 				return True
 
 def _check_result(result):
-	if len(result) is 0:
+	successful = len(result) is 0
+	no_such_zone = len(result) is 3 and result[2] == 'no_such_zone'
+	up_to_date = (len(result) is 3 and \
+		result[2] == 'record_already_exists_remove_first')
+
+	if successful:
 		print "Success"		
-	elif len(result) is 3 and result[2] == 'no_such_zone':
+	elif no_such_zone:
 		print "\nPlease visit your Dreamhost control panel, go to " 
 		print "Manage Domain and add an 'A' record of '0.0.0.0' "
 		print "and save. Then reload this script."
 		print "\nThis is an artifact of the DH API. Please email Dreamhost"
 		print "if this is an inconvience (it is)."
-	elif len(result) is 3 and result[2] == 'record_already_exists_remove_first':
+	elif up_to_date:
 		print "'A' record is up to date and points to your IP."
 	else:
 		print result
@@ -65,22 +68,20 @@ def _check_result(result):
 	print "\n"
 
 def update_ip(server=None, key=None, ipaddr=None, connection=None):
-	if server is None:
-		server = "yourdomain.com"
-	if key is None: 
-		key = "YOURKEYGOESHERE"
-	if ipaddr is None: 
-		ipaddr = _grab_ip()
-
 	if connection is not None:
 		server = connection._user
 		key = connection._key
 	else:
+		if server is None:
+			server = "yourdomain.com"
+		if key is None: 
+			key = "YOURKEYGOESHERE"
 		connection = _connect_api(server, key)	
-		
 
+	if ipaddr is None: 
+		ipaddr = _grab_ip()
+	
 	print "-> Server: ", server, " Key: ", key, " IP: ", ipaddr
-
 	result = connection.dns.add_record(record=str(server), value=str(ipaddr), type="A")
 	_check_result(result)
 
@@ -88,53 +89,78 @@ def update_via_csv(csvfile=None):
 	if csvfile is None:
 		csvfile = domain_file
 	
-	updateable_servers = []
+	ipaddr, updateable_servers = _grab_ip(), list()
 
 	with open(domain_file, 'r') as domain_csv:
 		reader = csv.reader(domain_csv, delimiter=delim)
 
 		for row in reader:
 			server, key = row[0], row[1]
-			connection, ipaddr = _connect_api(server, key), _grab_ip()	
+			connection = _connect_api(server, key)	
 			updateable = _check_record(connection, server, ipaddr)
 
 			if updateable is True:
 				updateable_servers.append(connection)
-				
-
-	domain_csv.close()
 
 	for connection in updateable_servers:
 		update_ip(connection=connection)
 	
-	if _to_be_culled() is True:
+	if len(to_be_culled) is not 0:
 		for tup in to_be_culled:
 			connection, server, value = tup
-			connection.dns.remove_record(record=server, value=value, type='A')
-		
+			connection.dns.remove_record(record=server, value=value, type='A')		
+
 def main():
 	import argparse
+
 	parser = argparse.ArgumentParser(description="Update your DreamHost DNS records.")
-	parser.add_argument('-f', 
-			    nargs=1,
-			    metavar='FILE', 
-			    type=str, 
-			    help='Comma-separated server/key file.',
-			    default=domain_file,
-			    dest='file')
+	parser.add_argument(
+				'-f', 
+				dest='file'
+			    	nargs=1,
+			   	metavar='FILE', 
+			    	type=str, 
+			    	help='Comma-separated server/key file.',
+			   	default=domain_file  )
 	
-	parser.add_argument('-s', dest='server', nargs=1, metavar='SERVER', type=str, help="Server's domain name")
-	parser.add_argument('-k', dest='key', nargs=1, metavar='KEY', type=str, help= "DreamHost API Key")
-	parser.add_argument('-ip', dest='ip', nargs=1, metavar='IP ADDRESS', type=str, help="Desired A record value")
+	parser.add_argument(
+				'-s', 
+				dest='server', 
+				nargs=1, 
+				metavar='SERVER', 
+				type=str, 
+				help="Server's domain name'  )
+
+	parser.add_argument(
+				'-k',
+				dest='key', 
+				nargs=1, 
+				metavar='KEY', 
+				type=str, 
+				help= "DreamHost API Key"  )
+	parser.add_argument(
+				'-ip', 
+				dest='ip', 
+				nargs=1, 
+				metavar='IP ADDRESS', 
+				type=str, 
+				help="Desired A record value"  )
+
 	a = parser.parse_args()
 	
-	if a.server is not None and a.key is not None and a.ip is not None:
+	manual_update =  \
+		a.server is not None and a.key is not None and a.ip is not None
+
+	insufficient_values_passed = \
+		a.server is not None or a.key is not None or a.ip is not None
+	
+	if manual_update:
 		update_ip(server=a.server[0], key=a.key[0], ipaddr=a.ip[0])
-	elif a.server is not None or a.key is not None or a.ip is not None:
-		print "Please specify a server, key, and IP address value"
+	elif insufficient_values_passed:
+		print "Please specify all values: server, key, and IP address"
 	else:
 		update_via_csv(csvfile=a.file)
 
 			
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
