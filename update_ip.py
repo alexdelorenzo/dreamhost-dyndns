c1261a99e7a855343ad5ec13ab96084cb05b9fe6
root <root@exynos.lan>
2013-07-18 01:55:27 +0000

    functions accept and reuse connection objects

--- update_ip.py
+++ update_ip.py
@@ -4,6 +4,7 @@ import csv
 
 domain_file = 'domains.csv'
 delim = ','
+to_be_culled = []
 
 def _grab_ip():
 	ipaddr = getip.get_external_ip()
@@ -13,26 +14,55 @@ def _connect_api(server, key):
 	connection = dhlib._connect(server, key)
 	print "Connected to DH API: ", connection.IsConnected(), "(", server, ")"
 	if connection.IsConnected() is False:
-		print "Not able to connect. Check your server name and API key values"
+		print "Not able to connect. Check your server name and API key values."
 		exit()
+
 	return connection
+
+def _to_be_culled(connection=None, record=None, value=None):
+	if record is None or value is None or connection is None:
+		culling_inevitable = True if len(to_be_culled) is 0 else False
+		return culling_inevitable
+	else:
+		to_be_culled.append((connection, record, value))
 
 def _check_record(connection, server, ipaddr):
 	records = connection.dns.list_records()
+
+	no_records = True if len(records) is 0 else False	
+	
+	if no_records:
+		return True
+
 	for record in records:
-		if record['zone'].strip() == str(server) and record['value'].strip() == str(ipaddr):
-			return False
-		if record['zone'].strip() == str(server) and record['value'].strip() != str(ipaddr):
-			return True
+		try:
+			name, value = record['record'], record['value']
+		except TypeError:
+			print records
+		else:
+			if name == server and value == str(ipaddr):
+				return False
+			if name == server and value != str(ipaddr):
+				_to_be_culled(connection, record, value)
+				return True
+			else:
+				return True
+
 def _check_result(result):
-	print result
-	if result[2] == 'no_such_zone':
-		print "Please visit your Dreamhost control panel, go to Manage Domain and add an 'A' record of '0.0.0.0' and save. Then reload this script."
-		print "\nThis is an artifact of the DH API. Please email them if this is an inconvience"
-	elif result[2] == 'record_already_exists_remove_first':
-		print "'A' record is up to date and points to your IP.\n"
+	if len(result) is 0:
+		print "Success"		
+	elif len(result) is 3 and result[2] == 'no_such_zone':
+		print "\nPlease visit your Dreamhost control panel, go to " 
+		print "Manage Domain and add an 'A' record of '0.0.0.0' "
+		print "and save. Then reload this script."
+		print "\nThis is an artifact of the DH API. Please email Dreamhost"
+		print "if this is an inconvience (it is)."
+	elif len(result) is 3 and result[2] == 'record_already_exists_remove_first':
+		print "'A' record is up to date and points to your IP."
 	else:
 		print result
+	
+	print "\n"
 
 def update_ip(server=None, key=None, ipaddr=None, connection=None):
 	if server is None:
@@ -40,14 +70,16 @@ def update_ip(server=None, key=None, ipaddr=None, connection=None):
 	if key is None: 
 		key = "YOURKEYGOESHERE"
 	if ipaddr is None: 
-		ipaddr = _grab_ip
-	if connection is None:
+		ipaddr = _grab_ip()
+
+	if connection is not None:
+		server = connection._user
+		key = connection._key
+	else:
 		connection = _connect_api(server, key)	
 		
 
-	print "Server: ", server, " Key: ", key
-	print "Externel IP: ", ipaddr
-
+	print "-> Server: ", server, " Key: ", key, " IP: ", ipaddr
 
 	result = connection.dns.add_record(record=str(server), value=str(ipaddr), type="A")
 	_check_result(result)
@@ -56,22 +88,30 @@ def update_via_csv(csvfile=None):
 	if csvfile is None:
 		csvfile = domain_file
 	
-	
-	updateable_servers = {}
+	updateable_servers = []
 
 	with open(domain_file, 'r') as domain_csv:
 		reader = csv.reader(domain_csv, delimiter=delim)
+
 		for row in reader:
 			server, key = row[0], row[1]
 			connection, ipaddr = _connect_api(server, key), _grab_ip()	
 			updateable = _check_record(connection, server, ipaddr)
+
 			if updateable is True:
-				updateable_servers[str(server)] = key
+				updateable_servers.append(connection)
+				
 
 	domain_csv.close()
-	for server, key in updateable_servers.iteritems():
-		update_ip(server, key, ipaddr)
 
+	for connection in updateable_servers:
+		update_ip(connection=connection)
+	
+	if _to_be_culled() is True:
+		for tup in to_be_culled:
+			connection, server, value = tup
+			connection.dns.remove_record(record=server, value=value, type='A')
+		
 def main():
 	import argparse
 	parser = argparse.ArgumentParser(description="Update your DreamHost DNS records.")
@@ -80,7 +120,7 @@ def main():
 			    metavar='FILE', 
 			    type=str, 
 			    help='Comma-separated server/key file.',
-			    default='domains.csv',
+			    default=domain_file,
 			    dest='file')
 	
 	parser.add_argument('-s', dest='server', nargs=1, metavar='SERVER', type=str, help="Server's domain name")
